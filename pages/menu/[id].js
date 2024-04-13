b8579a69fcd6e76109a906b28a741fdab41c2aa5
Ali Ghaffari <aligh71@yahoo.com>
2024-04-13 20:50:58 +0330

    create details page with dynamic route

---
+++ pages/menu/[id].js
@@ -0,0 +1,45 @@
+import DetailsPage from "@/components/templates/DetailsPage";
+import { useRouter } from "next/router";
+
+function Details({ data }) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <h2>Loading Page...</h2>;
+  }
+  return <DetailsPage {...data} />;
+}
+
+export default Details;
+
+export async function getStaticPaths() {
+  const res = await fetch("http://localhost:4000/data");
+  const json = await res.json();
+  const data = json.slice(0, 10);
+
+  const paths = data.map((food) => ({ params: { id: food.id.toString() } }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps(context) {
+  const {
+    params: { id },
+  } = context;
+  const res = await fetch(`http://localhost:4000/data/${id}`);
+  const data = await res.json();
+
+  if (!data.id) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { data },
+    revalidate: 10,
+  };
+}
